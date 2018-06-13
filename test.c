7a4f8c8ed2d7b50a13bf05eae542b2f898e102bc
DamarisMenfer <damaris.menfer@gmail.com>
2018-06-13 22:35:49 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,10 +1,28 @@
 int main(int arg, int arg2) {
 	int i, j,k,r;
+	int q;
 	i =3;
+	q = 1;
 	j=4;
 	k=8;
 	r = 0;
 	r++;
 	k = r +1;
-	r=(i+j)*(i+k/j);
+
+
+	if(i < j){
+		k += 2;
+	}
+	else if(q == i){
+		j++;
+	}
+	else{
+		i = 0;
+	}
+	
+
+	while(k < j){
+		k++;
+		j=k;
+	}
 }
